e8ab9b91062d7bab8b67792f82768f2401ef15c4
lwbin <604833953@qq.com>
2016-10-16 20:41:54 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-if (preg_match("/(错误)/", "系123123213统213错123213系统错误提示", $matches)) {
+if (preg_match("/(错误)/", "系00000统213错123213系统错误提示", $matches)) {
     print_r($matches);
 } else {
     echo "$test_name: no redirection\n";
